f4182f325cd903b80102b72cfc5444f75f30ac8d
kangjisung <kangjisung@naver.com>
2017-06-23 21:23:02 +0900

    Revert "Class:PaintChatServer"
    This reverts commit 0673ce45ac269bf99072fa718ee596b3b279c01b.

--- PaintChatServer/src/PaintChatServer.java
+++ PaintChatServer/src/PaintChatServer.java
@@ -1,67 +1,15 @@
+import kotlin.Unit;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-
-class EchoThread extends Thread{
-
-    private ArrayList<ObjectOutputStream> list; //
-
-    private ObjectInputStream objectInputStream;
-    private ObjectOutputStream objectOutputStream;
-    private String name;
-
-    public EchoThread(ArrayList<ObjectOutputStream> list,ObjectInputStream objectInputStream,ObjectOutputStream objectOutputStream){
-        this.list=list;
-        this.objectInputStream=objectInputStream;
-        this.objectOutputStream=objectOutputStream;
-    }
-
-    public void run(){
-        try{
-            this.name=(String) objectInputStream.readObject();
-
-            ChatData chatData=new ChatData("["+name+"] 님이 입장하셨습니다.");
-            SendObject(chatData); //입장
-
-            while(true){
-                Object object =objectInputStream.readObject(); //입력스트림으로 부터 데이터 수신
-
-                SendObject(object);//대화 내용
-            }
-        }catch (Exception e){
-            //if(object==null){
-                ChatData chatData=new ChatData("["+name+"] 님이 퇴장하셨습니다.");
-                SendObject(chatData);//퇴장
-                list.remove(objectOutputStream);
-                //objectInputStream.close();
-                //objectOutputStream.close();
-
-                //break;
-           // }
-        }
-    }
-
-    //대화 내용 클라이언트에 보내기
-    public void SendObject(Object object){
-        try {
-            for (int i = 0; i < list.size(); i++) {
-                list.get(i).writeObject(object);  // ChatData Object 전송;
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-}
+import java.util.HashMap;
 
 public class PaintChatServer {
 
     public static final int PORT = 7777;    //서버 포트
     public static void main(String args[]) {
         ServerSocket serverSocket = null;
-
-        ArrayList<ObjectOutputStream> list=new ArrayList<ObjectOutputStream>();
-
         try {
             serverSocket = new ServerSocket(PORT);  //서버 소켓 생성
             while(true){
@@ -69,17 +17,22 @@ public class PaintChatServer {
 
                 InputStream inputStream=socket.getInputStream(); //입력스트림 생성
                 ObjectInputStream objectInputStream=new ObjectInputStream(inputStream); //Object 입력 스트림 생성
+                Object object =objectInputStream.readObject(); //입력스트림으로 부터 데이터 수신
+
+                ChatData chatData=(ChatData)object; //클라이언트에 받은 데이터를 (chatData)object 형으로 입력
 
                 OutputStream outputStream = socket.getOutputStream();   //출력스트림 생성
                 ObjectOutputStream objectOutputStream=new ObjectOutputStream(outputStream); //Object 를 출력할 스트림 생성
+                objectOutputStream.writeObject(chatData);  // ChatData Object 전송
                 //objectOutputStream.writeObject(new PaintData(10,20,30));
 
-                list.add(objectOutputStream);
-
-                new EchoThread(list,objectInputStream,objectOutputStream).start();
-
+                objectInputStream.close();
+                objectOutputStream.close(); //스트림 종료
+                socket.close(); //소켓 종료
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
             e.printStackTrace();
         }
     }
